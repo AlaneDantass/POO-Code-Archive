a05022409ff945396e7f346740989c4e96931b42
Alane Dantas <alane.programming1@gmail.com>
2024-03-20 20:16:29 -0300

    Adicionei o exercício 02 e mudei o nome do exercício"Professor"
    Mudei o nome dos arquivos colocando a numeração de cada exercício ao lado do nome dele

---
+++ Códigos/Exercícios/Porta-02/Porta.java
@@ -0,0 +1,57 @@
+package POO.Exercícios.Porta;
+
+public class Porta {
+    private String cor;
+    private float altura;
+    private float largura;
+    private boolean estaAberta;
+
+    public Porta(String cor, float altura, float largura, boolean estaAberta) {
+        this.cor = cor;
+        this.altura = altura;
+        this.largura = largura;
+        this.estaAberta = estaAberta;
+    }
+
+    public String get_Cor(){
+        return cor;
+    }
+    public float get_Altura(){
+        return altura;
+    }
+    public float get_Largura(){
+        return largura;
+    }
+    public boolean get_EstaAberta(){
+        return estaAberta;
+    }
+    public void set_Cor(String Cor){
+        this.cor = Cor;
+    }
+    public void set_Altura(float Altura){
+        this.altura = Altura;
+    }
+    public void set_Largura(float Largura){
+        this.largura = Largura;
+    }
+    public void set_estaAberta(boolean EstaAberta){
+        this.estaAberta = EstaAberta;
+    }
+
+    public void abrir(){
+        estaAberta = true;
+    }
+
+    public void fechar(){
+        estaAberta = false;
+    }
+    public void imprimir(){
+        System.out.println("Cor da porta: " + get_Cor() + "\nAltura da porta: " + get_Altura() + "\nLargura da porta: " + get_Largura() + "\nA porta está aberta? " + get_EstaAberta());
+
+    }
+}
+
+//Defina uma classe Porta com os atributos: cor, altura, largura e estaAberta. Inclua na classe um construtor, os métodos gets e sets e o método toString() para retornar a representação textual do objeto.
+
+//Crie uma classe principal para criar objetos e invocar (chamar) os métodos disponíveis na classe Porta.
+
---
+++ Códigos/Exercícios/Porta-02/principal.java
@@ -0,0 +1,15 @@
+package POO.Exercícios.Porta;
+
+public class principal {
+    public static void main(String[] args) {
+        Porta porta1 = new Porta("Branca", 2.0f, 0.8f, false);
+        Porta porta2 = new Porta("Azul", 1.5f, 0.7f, true);
+    
+        porta1.abrir();
+        porta2.fechar();
+
+        porta1.set_Cor("Marrom");
+        porta1.imprimir();
+    
+    }
+}
